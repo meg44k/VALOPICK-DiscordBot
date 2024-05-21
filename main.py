82a93fecddf39ecb9d6675ea3643c87f1fb9d30b
meg44k <yugomegane@gmail.com>
2024-05-22 00:24:56 +0900

    Edited Path of image

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ async def pick(ctx:discord.Integration):
     charactor_list = ["KAY-O","アストラ","ヴァイパー","オーメン","キルジョイ","サイファー","ジェット","スカイ","セージ","チェンバー","ネオン","ハーバー","フェイド","フェニックス","ブリーチ","ブリムストーン","ヨル","レイズ","レイナ","ソーヴァ","ゲッコー","デッドロック","アイソ","クローヴ"]
     charactor_length = len(charactor_list)
     random_charactor = random.randint(0,charactor_length - 1)
-    await ctx.response.send_message("あなたが使うキャラクターは**【"+charactor_list[random_charactor]+"】**です。\n",file=discord.File("/image/"+str(random_charactor)+".png"))
+    await ctx.response.send_message("あなたが使うキャラクターは**【"+charactor_list[random_charactor]+"】**です。\n",file=discord.File("image/"+str(random_charactor)+".png"))
 
 @tree.command(
     name="mappic",
